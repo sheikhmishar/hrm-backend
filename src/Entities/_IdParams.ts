7e033cdf955ee689550484779f1b36f02c306fb4
sheikhmishar <misharex.sezan@gmail.com>
2024-11-17 10:54:55 +0600

    added ts validation with params

--- src/Entities/_IdParams.ts
+++ src/Entities/_IdParams.ts
@@ -1,13 +1,18 @@
 import { IsNumber, IsPositive } from 'class-validator'
 
+import SITEMAP from '../controllers/_routes/SITEMAP'
+
 export default class IdParams {
   @IsNumber()
   @IsPositive()
-  id!: number // TODO: get param name from _query
+  id!: number
 }
+
+SITEMAP.users._params satisfies { [k in keyof IdParams]: string }
 
 export class EmployeeIdParams {
   @IsNumber()
   @IsPositive()
-  employeeId!: number // TODO: get param name from _query
+  employeeId!: number
 }
+SITEMAP.attendances._params satisfies { [k in keyof EmployeeIdParams]: string }
